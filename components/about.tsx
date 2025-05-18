e553576d2197c62bea2cdf29684c496461785c0a
bouakram <kikoua203@gmail.com>
2025-05-18 21:40:59 +0100

    fixing ' issue

--- components/about.tsx
+++ components/about.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import SectionHeading from "./section-heading";
-import { useActiveSectionContext } from "@/context/active-section-context";
-import { useInView } from "react-intersection-observer";
 import { useInViewSection } from "@/lib/hooks";
 
 export default function About() {
@@ -23,7 +21,7 @@ export default function About() {
       <p className="mb-3">
         Hi, I’m <span className="font-medium dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-[#fba0c5] dark:to-[#a694ff]">Boughazi Akram</span> a developer from Algeria with a passion for <span className="font-medium dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-[#fba0c5] dark:to-[#a694ff]">solving problems</span> and turning ideas into real, usable products. I love building <span className="font-medium dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-[#fba0c5] dark:to-[#a694ff]">mobile apps</span> and <span className="font-medium dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-[#fba0c5] dark:to-[#a694ff]">websites</span>, and I’m always exploring <span className="font-medium dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-[#fba0c5] dark:to-[#a694ff]">new technologies</span>, tools, and concepts to keep learning and improving.
 
-        In my spare time, I dive into <span className="font-medium dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-[#fba0c5] dark:to-[#a694ff]">research</span>, especially in areas like <span className="font-medium dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-[#fba0c5] dark:to-[#a694ff]">cybersecurity</span> and <span className="font-medium dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-[#fba0c5] dark:to-[#a694ff]">blockchain</span>, which I find both challenging and inspiring. I'm also driven by <span className="font-medium dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-[#fba0c5] dark:to-[#a694ff]">collaboration</span> I enjoy meeting like-minded people, sharing <span className="font-medium dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-[#fba0c5] dark:to-[#a694ff]">knowledge</span>, and <span className="font-medium dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-[#fba0c5] dark:to-[#a694ff]">working together</span>to create smart solutions to complex problems.
+        In my spare time, I dive into <span className="font-medium dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-[#fba0c5] dark:to-[#a694ff]">research</span>, especially in areas like <span className="font-medium dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-[#fba0c5] dark:to-[#a694ff]">cybersecurity</span> and <span className="font-medium dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-[#fba0c5] dark:to-[#a694ff]">blockchain</span>, which I find both challenging and inspiring. I{"'"}m also driven by <span className="font-medium dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-[#fba0c5] dark:to-[#a694ff]">collaboration</span> I enjoy meeting like-minded people, sharing <span className="font-medium dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-[#fba0c5] dark:to-[#a694ff]">knowledge</span>, and <span className="font-medium dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-r dark:from-[#fba0c5] dark:to-[#a694ff]">working together</span>to create smart solutions to complex problems.
       </p>
     </motion.section>
   );
